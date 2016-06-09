daf53223e51b58590374f0a2dbd61e0846b3f532
Oceanswave <sean.mclellan@sixconcepts.com>
2016-06-08 21:24:38 -0400

    more logic

--- app.js
+++ app.js
@@ -9,42 +9,58 @@ powerSaveBlocker.start('prevent-app-suspension');
 
 const requireUncached = require('require-uncached');
 
-var clicky = null;
-
+let Clicky = null;
+let clicky = null;
+           
 // app.dock is not defined when running
 // electron in a platform other than OS X
 if (app.dock) {
     app.dock.hide();
 }
 
-app.on('ready', function() {
-
-    var ret = globalShortcut.register('`', function() {
+app.on('ready', function () {
+ 
+    var ret = globalShortcut.register('`', function () { 
         if (clicky) {
             clicky.stopClicky();
             clicky = null;
+            Clicky = null;
         }
         else {
-            let Clicky = requireUncached("./clicky.js").Clicky;
+            Clicky = requireUncached("./clicky.js").Clicky;
             clicky = new Clicky();
             clicky.startClicky();
         }
     });
 
-    globalShortcut.register('Shift+`', function() {
+    globalShortcut.register('Shift+`', function () { 
         let Clicky = requireUncached("./clicky.js").Clicky;
         console.log(Clicky.getMousePos());
     });
+
+    for (let i = 0; i < 9; i++) {
+        globalShortcut.register((i + 1).toString(), function () {
+            toggleClickySelector(i);
+        });
+    } 
+
+    let toggleClickySelector = function (ix) { 
+        if (!Clicky)
+            return;
+
+        console.log(`Toggling Selector ${ix + 1}`);
+        Clicky.toggleSelector(ix);
+    };
 
     if (ret) {
         console.log("ready");
     }
     else {
         console.log('registration failed');
-    }
+    } 
 });
 
-app.on('will-quit', function() {
+app.on('will-quit', function () {
 
     if (clicky) {
         clicky.stopClicky();
--- clicky.js
+++ clicky.js
@@ -2,39 +2,50 @@
 const co = require("co");
 const robot = require("robotjs");
 const delay = require("delay");
+const moment = require("moment");
+
+//this.topLocation = { x: 696, y: 330 }; //Where the "perk" location is.
+const TopLocation = { x: 1169, y: 330 }; // Horizontal "perk" button
+
+const Selectors = [
+    //Left Selectors
+    { x: 696, y: 480, enabled: true },
+    { x: 696, y: 720, enabled: true },
+    { x: 696, y: 930, enabled: true },
+    { x: 696, y: 1160, enabled: true },
+    //Right Selectors
+    { x: 1577, y: 265, enabled: true },
+    { x: 1577, y: 480, enabled: true },
+    { x: 1577, y: 720, enabled: true },
+    { x: 1577, y: 930, enabled: true },
+    { x: 1577, y: 1160, enabled: true }
+];
+
+//The thinks you press to make the monies.
+const Locations = [
+    //Bonus Activation
+    //{x: 1325, y: 350 },
+
+    //Left Monies
+    { x: 1025, y: 480, enabled: false },
+    { x: 1025, y: 720, enabled: false },
+    { x: 1025, y: 930, enabled: false },
+    { x: 1025, y: 1160, enabled: false },
+    //Right Monies
+    { x: 1975, y: 265, enabled: false, delay: 16 },
+    { x: 1975, y: 480, enabled: false, delay: 20 },
+    { x: 1975, y: 720, enabled: false, delay: 35 },
+    { x: 1975, y: 930, enabled: false, delay: 25 },
+    { x: 1975, y: 1160, enabled: false, delay: 32 },
+    //Left Buys
+    // {x: 1025, y: 602 }
+    //Right Buys
+    //{ x: 1950, y: 1280 }
+];
 
 class Clicky {
     constructor() {
         this._shouldStop = false;
-
-        this.topLocation = { x: 696, y: 330 }; //Where the "perk" location is. x: 1169 for the horizontal button.
-        this.locations = [
-            //Bonus Activation
-            {x: 1325, y: 350 },
-            //Left Selectors
-            //{ x: 696, y: 480 },
-            //{ x: 696, y: 720 },
-            //{ x: 696, y: 930 },
-            //{ x: 696, y: 1160 },
-            //Right Selectors
-            //{ x: 1577, y: 265 },
-            //{ x: 1577, y: 480 },
-            //{ x: 1577, y: 720 },
-            //{ x: 1577, y: 930 },
-            //{ x: 1577, y: 1160 }
-            //Left Monies
-           //{ x: 1025, y: 720 }
-           //{ x: 1025, y: 930 }
-            //Right Monies
-            //{ x: 1975, y: 265 }
-            //{ x: 1975, y: 720 },
-            //{ x: 1975, y: 930 },
-            { x: 1975, y: 1160 },
-            //Left Buys
-            // {x: 1025, y: 602 }
-            //Right Buys
-            { x: 1950, y: 1280 }
-        ];
         this.currentIndex = 0;
     }
 
@@ -42,58 +53,76 @@ class Clicky {
         return Math.floor(Math.random() * (high - low) + low);
     }
 
-    startClicky()
-    {
+    startClicky() {
         console.log("starting clicky...");
         this._shouldStop = false;
 
-        var fnClick = co.wrap(function*(self){
+        var fnClick = co.wrap(function* (self) {
             try {
-                while(!self._shouldStop) {
+                while (!self._shouldStop) {
                     yield self.click();
                 }
             }
-            catch(ex) {
+            catch (ex) {
                 throw ex;
             }
         });
 
-        fnClick(this).then(function() {
-        }, function(err){
+        fnClick(this).then(function () {
+        }, function (err) {
             console.log(err);
         });
 
     }
 
-    stopClicky()
-    {
+    stopClicky() {
         console.log("stopping clicky...");
         this._shouldStop = true;
     }
 
-    *click()
-    {
-        if (this.locations.length > 0) {
-            var current = this.locations[this.currentIndex];
+    *click() {
+        let currentSelector = Selectors[this.currentIndex];
+        let currentLocation = Locations[this.currentIndex];
 
-            robot.moveMouse(current.x, current.y);
+        if (currentSelector.enabled) {
+            robot.moveMouse(currentSelector.x, currentSelector.y);
             yield delay(1);
             robot.mouseClick();
             robot.keyTap("space");
         }
-        
-        robot.moveMouse(this.topLocation.x, this.topLocation.y);
+
+        robot.moveMouse(TopLocation.x, TopLocation.y);
         yield delay(1);
         robot.mouseClick();
         robot.keyTap("space");
 
+        if (currentLocation.enabled) {
+            let shouldClick = true;
+
+            if (currentLocation.nextClick)
+                shouldClick = moment().isAfter(currentLocation.nextClick);
+
+            if (shouldClick) {
+                robot.moveMouse(currentLocation.x, currentLocation.y);
+                yield delay(1);
+                robot.mouseClick();
+                //robot.keyTap("space");
+                currentLocation.nextClick = moment().add(currentLocation.delay, 'seconds');
+            }
+        }
+
         this.currentIndex++;
-        if (this.currentIndex > this.locations.length - 1)
+        if (this.currentIndex > Locations.length - 1) {
             this.currentIndex = 0;
+        }
     }
 
     static getMousePos() {
         return robot.getMousePos();
+    }
+
+    static toggleSelector(ix) {
+        Selectors[ix].enabled = !Selectors[ix].enabled;
     }
 }
 
